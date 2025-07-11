ea15fbb8911a309e38cbbb33bbb20b11bc272882
Russ Laher <laher@ipac.caltech.edu>
2025-07-11 10:40:27 -0700

    New method generateReferenceImageCatalog

---
+++ pipeline/gdip_referenceImageSubs.py
@@ -0,0 +1,37 @@
+import csv
+import re
+import boto3
+from botocore.exceptions import ClientError
+from astropy.io import fits
+import numpy as np
+
+import modules.utils.gdip_subs as util
+
+
+# Subs used by the general difference-image pipeline related to reference images and catalogs.
+
+
+#####################################################################################
+# Generate reference-image catalog and upload it to S3 bucket.
+#####################################################################################
+
+def generateReferenceImageCatalog(filename_refimage_image,
+                                  filename_refimage_uncert,
+                                  sextractor_refimage_dict,
+                                  filename_refimage_catalog):
+
+
+    # Compute SExtractor catalog for reference image.
+
+    sextractor_refimage_dict["sextractor_detection_image".lower()] = "None"
+    sextractor_refimage_dict["sextractor_input_image".lower()] = filename_refimage_image
+    sextractor_refimage_dict["sextractor_WEIGHT_IMAGE".lower()] = filename_refimage_uncert
+    sextractor_refimage_dict["sextractor_PARAMETERS_NAME".lower()] = "/code/cdf/rapidSexParamsRefImage.inp"
+    sextractor_refimage_dict["sextractor_FILTER_NAME".lower()] = "/code/cdf/rapidSexRefImageFilter.conv"
+    sextractor_refimage_dict["sextractor_STARNNW_NAME".lower()] = "/code/cdf/rapidSexRefImageStarGalaxyClassifier.nnw"
+    sextractor_refimage_dict["sextractor_CATALOG_NAME".lower()] = filename_refimage_catalog
+    sextractor_cmd = util.build_sextractor_command_line_args(sextractor_refimage_dict)
+    exitcode_from_sextractor = util.execute_command(sextractor_cmd)
+
+
+    return
