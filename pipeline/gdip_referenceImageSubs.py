5ed8b255cb17277c95602e59c92717f6a3723f32
Russ Laher <laher@ipac.caltech.edu>
2025-07-11 12:21:18 -0700

    Fixed cfg_path bugs.  Removed boto3 imports.  Added new method compute_coverage_map.

--- pipeline/gdip_referenceImageSubs.py
+++ pipeline/gdip_referenceImageSubs.py
@@ -1,7 +1,5 @@
 import csv
 import re
-import boto3
-from botocore.exceptions import ClientError
 from astropy.io import fits
 import numpy as np
 
@@ -27,12 +25,41 @@ def generateReferenceImageCatalog(filename_refimage_image,
     sextractor_refimage_dict["sextractor_detection_image".lower()] = "None"
     sextractor_refimage_dict["sextractor_input_image".lower()] = filename_refimage_image
     sextractor_refimage_dict["sextractor_WEIGHT_IMAGE".lower()] = filename_refimage_uncert
-    sextractor_refimage_dict["sextractor_PARAMETERS_NAME".lower()] = cfg_path + "/rapidSexParamsRefImage.inp"
-    sextractor_refimage_dict["sextractor_FILTER_NAME".lower()] = cfg_path + "/cdf/rapidSexRefImageFilter.conv"
-    sextractor_refimage_dict["sextractor_STARNNW_NAME".lower()] = cfg_path + "/rapidSexRefImageStarGalaxyClassifier.nnw"
+    sextractor_refimage_dict["sextractor_PARAMETERS_NAME".lower()] = cfg_path + "/srcExtractParamsRefImage.inp"
+    sextractor_refimage_dict["sextractor_FILTER_NAME".lower()] = cfg_path + "/srcExtractRefImageFilter.conv"
+    sextractor_refimage_dict["sextractor_STARNNW_NAME".lower()] = cfg_path + "/srcExtractRefImageStarGalaxyClassifier.nnw"
     sextractor_refimage_dict["sextractor_CATALOG_NAME".lower()] = filename_refimage_catalog
     sextractor_cmd = util.build_sextractor_command_line_args(sextractor_refimage_dict)
     exitcode_from_sextractor = util.execute_command(sextractor_cmd)
 
 
     return
+
+
+#####################################################################################
+# Compute reference-image coverage map.
+#####################################################################################
+
+
+def compute_coverage_map(input_filename,hdu_index,output_filename,nframes):
+
+    hdul = fits.open(input_filename)
+    hdr = hdul[hdu_index].header
+    data = hdul[hdu_index].data
+
+    hdul.close()
+
+    hdr["BUNIT"] = "counts"
+
+    np_data = np.array(data)            # Ensure data are positive for uncertainty calculations.
+
+    cov_map = np.where(np.isnan(np_data), 0.0, nframes)
+
+    hdu_cov = fits.PrimaryHDU(header=hdr,data=cov_map.astype(np.float32))
+    hdu_list_cov = []
+    hdu_list_cov.append(hdu_cov)
+    hdu_cov = fits.HDUList(hdu_list_cov)
+    hdu_cov.writeto(output_filename,overwrite=True,checksum=True)
+
+    return
+
